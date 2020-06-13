0c09861f5fb1454778a6882eb2169d41f2187337
becklin-haston <bwhaston@gmail.com>
2020-06-13 15:04:00 -0700

    commented out not needed import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-import pandas as pd
-from pdf2image import convert_from_path, convert_from_bytes
+import pandas
+import numpy
+# from pdf2image import convert_from_path, convert_from_bytes
 
